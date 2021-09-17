39d0c73b1795c7093cf5ab29b776fd3085752bc2
Petar Markov <riki7894@gmail.com>
2021-09-17 12:01:19 +0200

    hitni poslovi popravljeni

--- src/views/HitniPoslovi.vue
+++ src/views/HitniPoslovi.vue
@@ -1,96 +1,116 @@
 <template>
   <div class="container">
     <div class="row">
-        <div class="col-md-4">
-            <div class="card mt-3">
-                <div class="product-1 align-items-center p-2 text-center"> <img src="https://i.imgur.com/fcRasFv.jpg" class="rounded" width="160">
-                    <h5 class="title">Košnja trave</h5>
-                    <div class="mt-3 info"> <span class="text1 d-block">Potrebna osoba za košnju bla bla...</span> <span class="text1">cleansing water. 125 ml </span> </div>
-                    <div class=" cost mt-3 text-dark"> 
-                      <span>150Kn</span>
-                    </div>
-                </div>
-                <div class="p-3 bg-danger text-center text-white mt-3 cursor"> <span class="text-uppercase">Add to cart</span> </div>
+      <div class="col-md-4">
+        <div class="card mt-3">
+          <div class="product-1 align-items-center p-2 text-center">
+            <img
+              src="https://i.imgur.com/fcRasFv.jpg"
+              class="rounded"
+              width="160"
+            />
+            <h5 class="title">Košnja trave</h5>
+            <div class="mt-3 info">
+              <span class="text1 d-block"
+                >Potrebna osoba za košnju bla bla...</span
+              >
             </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card mt-3">
-                <div class="product-1 align-items-center p-2 text-center"> <img src="https://i.imgur.com/vD8fgks.jpg" class="rounded" width="160">
-                    <h5 class="title">Šetnja psa</h5>
-                    <div class="mt-3 info"> <span class="text1 d-block">Šetnja umiljatog psa rexa bla bla..</span> <span class="text1">cleansing water.</span> </div>
-                    <div class=" cost mt-3 text-dark"> 
-                      <span>100Kn</span>
-                    </div>
-                </div>
-                <div class="watch p-3 text-center text-white mt-3 cursor"> <span class="text-uppercase ">Add to cart</span> </div>
+            <div class="cost mt-3 text-dark">
+              <span>150Kn</span>
             </div>
+          </div>
+          <div class="product-1 p-3 text-center text-white mt-3 cursor">
+            <input class="btn" type="button" value="Preuzmi posao" />
+          </div>
         </div>
-        <div class="col-md-4">
-            <div class="card mt-3">
-                <div class="product-1 align-items-center p-2 text-center"> <img src="https://i.imgur.com/Iy7w1yJ.jpg" class="rounded" width="160">
-                    <h5 class="title">Kopanje</h5>
-                    <div class="mt-3 info"> <span class="text1 d-block">Kopavnje vrta 100 kvadrata bla bla..</span> <span class="text1">cleansing water. 125 ml </span> </div>
-                    <div class=" cost mt-3 text-dark"> 
-                      <span>200Kn</span>
-                    </div>
-                </div>
-                <div class=" tshirt p-3 text-center text-white mt-3 cursor"> <span class="text-uppercase">Add to cart</span> </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card mt-3">
+          <div class="product-1 align-items-center p-2 text-center">
+            <img
+              src="https://i.imgur.com/fcRasFv.jpg"
+              class="rounded"
+              width="160"
+            />
+            <h5 class="title">Košnja trave</h5>
+            <div class="mt-3 info">
+              <span class="text1 d-block"
+                >Potrebna osoba za košnju bla bla...</span
+              >
             </div>
+            <div class="cost mt-3 text-dark">
+              <span>150Kn</span>
+            </div>
+          </div>
+          <div class="product-1 p-3 text-center text-white mt-3 cursor">
+            <input class="btn" type="button" value="Preuzmi posao" />
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <style scoped>
+  .title {
+    color: white;
+    margin-top: 10px;
+  }
 
-.title{
-  color: white;
-  margin-top: 10px;
-}
+  .product-1 {
+    margin-top: 10px;
+  }
 
-.product-1{
-  margin-top: 10px;
-}
-
-.row{
+  .row {
     height: 60%;
     width: 60%;
     position: absolute;
     top: 50%;
     left: 50%;
     margin-right: -50%;
-    transform: translate(-50%, -50%)
-}
-.card {
+    transform: translate(-50%, -50%);
+  }
+  .card {
     border: none;
     border-radius: 10px;
     background: rgba(0, 0, 0, 0.85);
-}
+  }
 
-.text1 {
+  .text1 {
     font-size: 15px;
-    color: #cbcbcb
-}
+    color: #cbcbcb;
+  }
 
-.info {
-    line-height: 17px
-}
+  .info {
+    line-height: 17px;
+  }
 
-.watch {
-    background: #286783
-}
+  .watch {
+    background: #286783;
+  }
 
-.cost span {
+  .cost span {
     color: white;
     font-weight: bold;
-    font-size: 20px
-}
+    font-size: 20px;
+  }
 
-.tshirt {
-    background-color: #0012b2
-}
+  .btn {
+    color: black;
+    width: auto;
+    border: 0;
+    background: none;
+    background-color: #ffc312;
+    display: block;
+    margin: 15px auto;
+    text-align: center;
+    outline: none;
+    border-radius: 24px;
+    transition: 0.25s;
+    cursor: pointer;
+  }
 
-.cursor {
-    cursor: pointer
-}
+  .cursor {
+    cursor: pointer;
+  }
 </style>
